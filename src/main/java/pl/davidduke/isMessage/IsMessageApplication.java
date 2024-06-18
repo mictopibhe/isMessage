5536e28da6d59599aa48bdb590fa2ccbe0d92496
Aleksander Davydiuk <mictopibhe@gmail.com>
2024-06-18 07:47:33 +0200

    First commit

---
+++ src/main/java/pl/davidduke/isMessage/IsMessageApplication.java
@@ -0,0 +1,13 @@
+package pl.davidduke.isMessage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IsMessageApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IsMessageApplication.class, args);
+	}
+
+}
